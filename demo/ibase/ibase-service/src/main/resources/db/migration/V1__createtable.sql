ddbf632029a746609ee4c2033b2c08bb80124f15
sunyandi <sunyd@inspur.com>
2018-09-22 18:04:52 +0800

    add flyway

---
+++ demo/ibase/ibase-service/src/main/resources/db/migration/V1__createtable.sql
@@ -0,0 +1,18 @@
+use springcloud;
+CREATE TABLE `t_ba_test` (
+  `id` varchar(64) NOT NULL,
+  `name` varchar(32) DEFAULT NULL,
+  `email` varchar(32) DEFAULT NULL,
+  `account` varchar(32) DEFAULT NULL,
+  `is_default` int(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `t_ba_syslog` (
+  `syslog_id` varchar(64) NOT NULL,
+  `user_id` varchar(64) DEFAULT NULL,
+  `domain_id` varchar(64) DEFAULT NULL,
+  `org_id` varchar(64) DEFAULT NULL,
+  `log_date` date DEFAULT NULL,
+  PRIMARY KEY (`syslog_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
