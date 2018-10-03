a704769724ca3aca9320f752a8ce8d22831f807d
sunyandi <sunyd@inspur.com>
2018-10-03 17:12:56 +0800

    up zuul filter

--- demo/incloudmanager-api-gateway/src/main/java/com/inspur/incloud/apigateway/Filter/IncloudZuulFilter.java
+++ demo/incloudmanager-api-gateway/src/main/java/com/inspur/incloud/apigateway/Filter/IncloudZuulFilter.java
@@ -1,8 +1,5 @@
 package com.inspur.incloud.apigateway.Filter;
 
-import java.util.Collection;
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang.StringUtils;
@@ -10,15 +7,16 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
-import org.springframework.cloud.netflix.zuul.filters.ZuulProperties.ZuulRoute;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.inspur.incloud.common.OperationResult;
 import com.inspur.incloud.iauth.client.model.user.UserInforModel;
 import com.inspur.incloud.iauth.client.tokens.TokensApi;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
-
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.ROUTE_TYPE;
 public class IncloudZuulFilter extends ZuulFilter {
 	
 	private static final Logger Logger = LoggerFactory.getLogger(IncloudZuulFilter.class);
@@ -30,34 +28,15 @@ public class IncloudZuulFilter extends ZuulFilter {
 	private TokensApi tokensApi;
 
 	public Object run() throws ZuulException {
+		boolean isSuccess = true;
+		Integer code = 200;
 		RequestContext context = RequestContext.getCurrentContext();
 		HttpServletRequest request = context.getRequest();
-		String lang = request.getHeader("lang");
         String host = request.getRemoteHost();
         Logger.info("print the host ip: " + host);
-        String url = request.getRequestURI();
-        if(StringUtils.isNotEmpty(url) && url.contains("/v2/api-docs")) {
-        	return null;
-        }
-        Collection<ZuulRoute> routers = properties.getRoutes().values();
-        boolean isNeedFilter = false;
-        for (ZuulRoute router : routers) {
-        	String path = router.getPath();
-        	if (StringUtils.isNotEmpty(path) && StringUtils.isNotEmpty(url)) {
-        		if (url.startsWith(path.substring(0, path.lastIndexOf("**")))) {
-        			isNeedFilter = true;
-        			break;
-        		}
-        	}
-        }
-        if (!isNeedFilter) {
-        	return null;
-        }
-        //验证token是否合法
-        boolean isSuccess = true;
-        Integer code = 200;
         String token = request.getHeader("X-Auth-Token");
         String keepAlive = request.getHeader("X-Auth-Keep-Alive");
+        String lang = request.getHeader("X-Accept-Language");
         Logger.debug("X-Auth-Token: " + token);
         Logger.debug("X-Auth-Keep-Alive: " + keepAlive);
         if(StringUtils.isEmpty(token)) {
@@ -85,15 +64,26 @@ public class IncloudZuulFilter extends ZuulFilter {
         	 } else {
         		 opeResult.setErrMessage("无效的token "+ token);
         	 }
-			context.setResponseBody(opeResult.toString());
-        } else {
-        	
+        	ObjectMapper mapper = new ObjectMapper();
+        	try {
+				String json = mapper.writeValueAsString(opeResult);
+				context.setResponseBody(json);
+			} catch (JsonProcessingException e) {
+				Logger.error(e.getMessage(), e);
+			}
         }
         return null;
 	}
 
 	public boolean shouldFilter() {
-		return true;
+		boolean isNeedFilter = true;
+		RequestContext context = RequestContext.getCurrentContext();
+		HttpServletRequest request = context.getRequest();
+        String url = request.getRequestURI();
+        if(StringUtils.isNotEmpty(url) && url.contains("/v2/api-docs")) {
+        	return false;
+        }
+		return isNeedFilter;
 	}
 
 	@Override
@@ -103,7 +93,7 @@ public class IncloudZuulFilter extends ZuulFilter {
 
 	@Override
 	public String filterType() {
-		return "pre";
+		return ROUTE_TYPE;
 	}
 
 }
